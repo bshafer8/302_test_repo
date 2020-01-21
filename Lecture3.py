1c3faebde2ee7e592ed4380e927d25601be3a421
Brandon Shafer <59841132+bshafer8@users.noreply.github.com>
2020-01-21 11:40:44 -0500

    Add files via upload

---
+++ Lecture3.py
@@ -0,0 +1,42 @@
+"""
+Author: Brandon Shafer
+Created: January 21, 2020
+Python Version: 3.7.4
+Time:
+Note: In class exercise for Lecture 3
+"""
+
+
+"""
+# This is some sample code
+message = "Python version: 3.x"
+n = 17
+pi = 3.14159
+
+print(type(message))
+print(type(n))
+print(type(pi))
+
+
+folder = "d:/data/ma/"
+file_1 = "roads.shp"
+road_layer = folder + file_1
+print(road_layer)
+
+
+my_string = "pointdata"
+print(my_string*3)
+
+
+print("Have you read \"Things Fall Apart\"?")
+"""
+
+pi = 3.14  # rounded off
+r = 2.0  # defining the radius
+area = pi * (r**2)  # formula for the area of a circle
+perim = 2.0 * pi * r  # formula for the perimeter of a circle
+
+#These lines output the results
+print("Given r: ", r)
+print("Area is: ", area)
+print("Perimeter is: ", perim)
